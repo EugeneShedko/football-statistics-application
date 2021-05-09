f19fc6eaec025b48e916496cd8605265edf47049
evgenij-2002@bk.ru <evgenij-2002@bk.ru>
2021-05-09 21:20:04 +0300

    Создание viewmodel для mainwindow Создание комманд

---
+++ WpfApp1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WpfApp1.Commands;
+
+namespace WpfApp1.ViewModels
+{
+	internal class MainWindowViewModel : ViewModelBase
+	{
+		#region Constructors
+		public MainWindowViewModel()
+		{
+			ShowWindow = new DelegateCommand(ShowWindowCommand, CanShowWindowCommand);
+		}
+		#endregion
+		#region Properties
+		private string _Login;
+		public string Login
+		{
+			get { return _Login;}
+			set 
+			{
+				Set(ref _Login, value);
+			}
+		}
+		private string _Password;
+		public string Password
+		{
+			get { return _Password; }
+			set
+			{
+				Set(ref _Password, value);
+			}
+		}
+		#endregion
+		#region Commands
+
+		public ICommand ShowWindow { get;}
+
+		public bool CanShowWindowCommand(object parameter)
+		{
+			return true;
+		}
+		public void ShowWindowCommand(object parameter)
+		{
+			Window s = new Window();
+			s.Show();
+		}
+		
+		#endregion
+	}
+}
---
+++ WpfApp1/ViewModels/ViewModelBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.ViewModels
+{
+	internal abstract class ViewModelBase : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)    //CallMemberName позволяет получить имя свойства
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+		}
+
+		protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+		{
+			if (Equals(field, value)) return false;
+			field = value;
+			OnPropertyChanged(PropertyName);
+			return true;
+		}
+	}
+}
